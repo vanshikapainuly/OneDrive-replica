c5d9c06832e91c4ccb24e49246e716f690a3097e
vanshikapainuly <111229667+vanshikapainuly@users.noreply.github.com>
2025-06-15 04:22:13 +0530

    QRdisplay.py

--- QRdisplay.py
+++ QRdisplay.py
@@ -4,24 +4,21 @@ import socketserver
 import webbrowser
 import pyqrcode
 from pyqrcode import QRCode
-import png
 import os
 
 PORT = 8010
-os.environ['USERPROFILE']
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']),
                         'OneDrive')
 os.chdir(desktop)
 
 Handler = http.server.SimpleHTTPRequestHandler
-hostname = socket.gethostname()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #trick
 s.connect(("8.8.8.8", 80))
 IP = "http://" + s.getsockname()[0] + ":" + str(PORT)
-link = IP
 
-url = pyqrcode.create(link)
+
+url = pyqrcode.create(IP)
 url.svg("myqr.svg", scale=8)
 webbrowser.open('myqr.svg')
 
